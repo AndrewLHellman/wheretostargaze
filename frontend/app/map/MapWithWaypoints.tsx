2f83ed70f0563ac44fe39472d28744141f7b831e
Zack Murry <zackk.murryy@gmail.com>
2025-11-07 22:28:13 -0600

    Add destinations

--- frontend/app/map/MapWithWaypoints.tsx
+++ frontend/app/map/MapWithWaypoints.tsx
@@ -1,44 +1,55 @@
 'use client'
 
-import React, { useState } from 'react'
-import { useUserLocation } from '@/lib/useUserLocation'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import React from 'react'
+import { MapContainer, TileLayer, Tooltip } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import UserMarker from '@/components/UserMarker'
+import LucideMarker from './LucideMarker'
+import { FlagTriangleRight } from 'lucide-react'
 import { SpotResponse } from '@/lib/types'
+import UserMarker from '@/components/UserMarker'
+import { useUserLocation } from '@/lib/useUserLocation'
 
 interface Props {
   data: SpotResponse | null
 }
 
 export default function MapWithWaypoints({ data }: Props) {
-  const { location: userLocation, error: userLocationError } = useUserLocation()
-  console.log(data)
+  const { location: userLocation } = useUserLocation()
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <MapContainer
         style={{ height: '100%', width: '100%' }}
         center={[38.9452, -92.3288]}
-        zoom={17}
+        zoom={11}
         attributionControl={false}
       >
         <TileLayer
           attribution='&copy; OpenStreetMap contributors &copy; CARTO'
-          // url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
           url='https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
         />
-        {/* User location marker */}
-        {userLocation && <UserMarker position={[userLocation.lat, userLocation.lng]} />}
-      </MapContainer>
 
-      {/* <Button
-        className='absolute bottom-5 right-5 p-2 bg-blue-500 text-white !z-[100000] cursor-pointer rounded-md'
-        loading={isLoading}
-      >
-        Generate Route
-      </Button> */}
+        {/* User location */}
+        {userLocation && <UserMarker position={[userLocation.lat, userLocation.lng]} />}
+
+        {/* Recommended spots */}
+        {data?.recommended_spots?.map((spot, i) => (
+          <LucideMarker
+            key={i}
+            position={[spot.lat, spot.lon]}
+            size={28}
+            color='#2563eb'
+            align='bottom'
+            LucideIcon={FlagTriangleRight}
+            onRightClick={() => alert(`Right-clicked: ${spot.name}`)}
+          >
+            <Tooltip direction='top' offset={[0, -10]} opacity={1} permanent={false}>
+              {spot.name}
+            </Tooltip>
+          </LucideMarker>
+        ))}
+      </MapContainer>
     </div>
   )
 }
